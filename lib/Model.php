a469f405f43fd04d4327f5fae7179c28a9b407a5
Sadik Younsse <ysc2011.freelancer@gmail.com>
2019-06-25 16:27:57 +0100

    Update Model.php
    Fix countable warning

--- lib/Model.php
+++ lib/Model.php
@@ -1597,8 +1597,9 @@ class Model
 			$num_args--;
 		}
 		//find by pk
-		elseif (1 === count($args) && 1 == $num_args)
-			$args = $args[0];
+		// fix the countable bug
+		/*elseif (1 === count($args) && 1 == $num_args)
+			$args = $args[0];*/
 
 		// anything left in $args is a find by pk
 		if ($num_args > 0 && !isset($options['conditions']))
